1c2b6461cb3f7e7f56b4ef1344ccbcdacfee2865
Otmane <Otmane@LAPTOP-RG67H8NT>
2023-10-31 00:16:01 +0100

    End of Section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using AuctionService.Entities;
+namespace AuctionService.Data;
+
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet <Auction> Auctions{get;set;}
+}
