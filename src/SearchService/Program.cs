d2b8e0f0e18916d8b540243ded5c8c34e3fef04d
Otmane <Otmane@LAPTOP-RG67H8NT>
2023-11-17 22:45:24 +0100

    End Of Section 7

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -17,18 +17,24 @@ builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolic
 builder.Services.AddMassTransit(x =>
 {
       x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
-      x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search",false));
+      x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
 
       x.UsingRabbitMq((context, cfg) =>
   {
-      cfg.ReceiveEndpoint("search-auction-created", e=>
-      {
-            e.UseMessageRetry(r => r.Interval(5,5));
-            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
-      });
+
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+              host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+              host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+              e.UseMessageRetry(r => r.Interval(5, 5));
+              e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
         cfg.ConfigureEndpoints(context);
   });
-});   
+});
 
 
 var app = builder.Build();
